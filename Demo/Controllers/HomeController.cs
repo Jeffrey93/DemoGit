3117d8655a0fcc649e088f4d6809fde61db8bc65
hady yeffrey chaverra <ingenierojeffry@hotmail.com>
2017-10-09 08:55:32 -0500

    Agregados archivos

---
+++ Demo/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var persona = new Persona()
+            {
+                Nombre = "Pedro",
+                Edad = 20,
+                Empleado = true,
+                Nacimiento = new DateTime(2017, 10, 10)
+            };
+
+            ViewBag.Persona = persona;
+            ViewBag.MiListado = ObtenerListado();
+            return View();
+        }
+        public List<SelectListItem> ObtenerListado(){
+            return new List<SelectListItem>()
+            {
+                   new SelectListItem()
+        {
+            Text = "Sí",
+            Value = "1" 
+        },
+        new SelectListItem()
+        {
+            Text = "No",
+            Value = "2"
+        }
+            };
+        }
+        public ActionResult About(int edad)
+            {
+            ViewBag.Message = "Tu edad es " +edad ;
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Pagina de contacto.";
+
+            return View();
+        }
+        public ActionResult Mapa()
+        {
+            return View();
+        }
+    }
+     public class Persona
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public bool Empleado { get; set; }
+        public DateTime Nacimiento { get; set; }
+
+    }
+}
